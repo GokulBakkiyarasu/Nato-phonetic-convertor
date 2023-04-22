cf52e8f093024d3ae0b0f39edfe19089464759b0
Gokul <87391223+GokulBakkiyarasu@users.noreply.github.com>
2023-04-22 20:45:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ import pandas
 nato_data = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dictionary = {row.letter: row.code for (index, row) in nato_data.iterrows()}
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter the word that has to be converted into a nato_phonetic word list: ").upper()
-nato_list = [nato_dictionary[letter] for letter in word]
-print(nato_list)
+
+
+def convertor():
+    word = input("Enter the word that has to be converted into a nato_phonetic word list: ").upper()
+    # used  in oder if the user have entered white along the beginning or ending of the word 
+    word = word.strip()
+    try:
+        nato_list = [nato_dictionary[letter] for letter in word]
+        print(nato_list)
+    except KeyError:
+        print("Sorry, please enter a letter in alphabet.")
+        convertor()
+
+
+convertor()
